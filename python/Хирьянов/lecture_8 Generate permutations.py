c28b3140f23018178bdab7e019ab78e7a6c4dc36
ZaharChernenko <zaharchernenko35@gmail.com>
2023-09-27 14:10:53 +0300

    changed lecture_8

--- python/Хирьянов/lecture_8 Generate permutations.py
+++ python/Хирьянов/lecture_8 Generate permutations.py
@@ -20,7 +20,11 @@ def generateNumbers(length: int, base: int = 10, prefix: str = "", arr=[]):
     return arr
 
 
-def generatePermutations(length: int, elements_list: list, prefix="", arr=[]):
+# в этой функции есть один косяк, который показывается лишь при неоднократном вызове функции,
+# проблема в том, что по дефолту функция создает массив один раз, а при следующем вызове функции, она
+# добавляет элементы в уже существующий массив, поэтому нужно либо явно указвать массив, либо сделать, как в
+# функции ниже
+def generatePermutationsUnexpected(length: int, elements_list: list, prefix="", arr=[]):
     assert len(elements_list) >= length, "Необходимо, чтобы число элементов было не меньше длины последовательности"
     if length == 0:
         arr.append(prefix)
@@ -32,6 +36,22 @@ def generatePermutations(length: int, elements_list: list, prefix="", arr=[]):
     return arr
 
 
+def generatePermutations(length: int, collection, prefix="", arr=None):
+    if arr is None:
+        arr = []
+    assert len(collection) >= length, "Необходимо, чтобы число элементов было не меньше длины последовательности"
+    if length == 0:
+        arr.append(prefix)
+        return
+    for elem in collection:
+        if str(elem) in prefix:
+            continue
+        generatePermutations(length - 1, collection, prefix + str(elem), arr)
+    return arr
+
+
 arr = generatePermutations(5, list(range(1, 6)))
 print(arr)
 print(len(arr))
+print(generatePermutations(3, "abcdef"))
+print(generatePermutations(3, "abcdef", "", []))

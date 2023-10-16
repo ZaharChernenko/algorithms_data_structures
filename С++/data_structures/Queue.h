c4d7e4f395f2372b9c321ebdcd2771a10bfe57b8
ZaharChernenko <zaharchernenko35@gmail.com>
2023-10-16 13:43:11 +0300

    Queue

--- С++/data_structures/Queue.h
+++ С++/data_structures/Queue.h
@@ -1,19 +1,71 @@
 #pragma once
 #include <iostream>
 
-template <typename T>
+
+template <class T>
 class Queue {
 private:
     struct Node {
         T val;
         Node* next;
+        Node();
         Node(T val);
     };
 
-    Node* _head, _tail;
+    Node* _head;
+    Node* _tail;
     std::size_t _size;
 
 public:
     Queue();
+
+    template <class T1> friend std::ostream& operator<<(std::ostream& os, const Queue<T1>& q);
+    std::size_t size() const;
     void pushBack(T val);
 };
+
+template <class T>
+Queue<T>::Node::Node():
+        next{nullptr} {}
+
+
+template <class T>
+Queue<T>::Node::Node(T val):
+        val{val}, next{nullptr} {}
+
+
+template <class T>
+Queue<T>::Queue():
+    _head{nullptr}, _tail{nullptr}, _size{0} {}
+
+
+template <class T1>
+std::ostream& operator<<(std::ostream& os, const Queue<T1>& q) {
+    auto temp = q._head;
+    for (std::size_t i = 0; i < q.size(); ++i) {
+        os << temp->val;
+        temp = temp->next;
+        if (i != q.size() - 1) os << ' ';
+    }
+    return os;
+}
+
+
+template <class T>
+std::size_t Queue<T>::size() const {
+    return _size;
+}
+
+
+template <class T>
+void Queue<T>::pushBack(T val) {
+    if (_head == nullptr) {
+        _tail = _head = new Node(val);
+        ++_size;
+    }
+    else {
+        _tail->next = new Node(val);
+        _tail = _tail->next;
+        ++_size;
+    }
+}
--- С++/data_structures/main.cpp
+++ С++/data_structures/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "Queue.h"
 
+using std::cin, std::cout;
+
 int main() {
-    Queue<int> q1;
-    q1.pushBack(1);
+    std::cout << "Hello, World!" << std::endl;
+    Queue<int> q;
+    q.pushBack(12);
+    q.pushBack(129);
+    cout << q;
     return 0;
 }

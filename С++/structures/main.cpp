3598e6251d956f13485d810f420334faec07481c
ZaharChernenko <zaharchernenko35@gmail.com>
2023-10-16 03:43:56 +0300

    now templates are working

--- С++/structures/main.cpp
+++ С++/structures/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "Queue.h"
 
+using std::cin, std::cout;
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     Queue<int> q;
+    q.pushBack(12);
+    q.pushBack(129);
+    cout << q;
     return 0;
 }

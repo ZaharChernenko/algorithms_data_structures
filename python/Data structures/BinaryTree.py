d183dac3d9cffc13b99d60312bb5aa5e3fac0832
ZaharChernenko <zaharchernenko35@gmail.com>
2023-10-30 01:09:01 +0300

    changed insert in BinaryTree.py

--- python/Data structures/BinaryTree.py
+++ python/Data structures/BinaryTree.py
@@ -13,12 +13,36 @@ class BinaryTree:
             self.__root = self.__Binary_Node(args[0])
             self.__size = 1
             for i in range(1, len(args)):
-                self.insert(args[i])
+                self.__insert(args[i])
 
     def __str__(self):
-        return str(self.to_list())
+        return str(self.__to_list())
 
-    def insert(self, val, root=None) -> None:
+    def __insert(self, val) -> None:
+        parent = None
+        cur_node = self.__root
+
+        while cur_node is not None:
+            parent = cur_node
+
+            if cur_node.val > val :
+                cur_node = cur_node.left
+            elif cur_node.val < val:
+                cur_node = cur_node.right
+            else:
+                return
+
+        if parent is None:
+            self.__root = self.__Binary_Node(val)
+        elif parent.val > val:
+            parent.left = self.__Binary_Node(val)
+        else:
+            parent.right = self.__Binary_Node(val)
+
+        self.__size += 1
+
+    """ Рекурсивная реализация вставки, по идее менее эффективная, чем цикл
+        def insert(self, val, root=None) -> None:
         if root is None:
             if self.__root is None:
                 self.__root = self.__Binary_Node(val)
@@ -39,15 +63,45 @@ class BinaryTree:
                 self.__size += 1
                 return
             self.insert(val, root.right)
+    """
 
-    def to_list(self, root=None, res=None) -> list:
-        if root is None:
+    def __to_list(self, cur_node=None, res=None) -> list:
+        if cur_node is None:
             res = []
             if self.__root is None: return res
-            root = self.__root
+            cur_node = self.__root
 
-        if root.left: self.to_list(root.left, res)
-        res.append(root.val)
-        if root.right: self.to_list(root.right, res)
+        if cur_node.left: self.__to_list(cur_node.left, res)
+        res.append(cur_node.val)
+        if cur_node.right: self.__to_list(cur_node.right, res)
 
         return res
+
+    def __remove(self, key, cur_node=None, is_zero_step=True) -> None:
+        if cur_node is None:
+            if not is_zero_step:
+                raise KeyError
+            cur_node = self.__root
+            is_zero_step = False
+
+        if cur_node.val == key:
+            if cur_node.left is not None and cur_node.right is not None:
+                temp = cur_node.right
+                if temp.left is None:
+                    cur_node.val = temp.val
+                    cur_node.right
+                while temp.left is not None:
+                    temp = temp.left
+
+
+        elif cur_node.val > key:
+            self.__remove(key, cur_node.left, is_zero_step)
+
+        else:
+            self.__remove(key, cur_node.right, is_zero_step)
+
+    def insert(self, val): # инкапсуляция необходима, чтобы пользователь не добавил какой-то неизвестный корень
+        self.__insert(val)
+
+    def to_list(self):
+        return self.__to_list()
--- python/Data structures/main.py
+++ python/Data structures/main.py
@@ -2,4 +2,5 @@ from MyQueue import MyQueue
 from BinaryTree import BinaryTree
 
 tree = BinaryTree(4, 3, 2, 1, 21)
+tree.insert(-1)
 print(tree)

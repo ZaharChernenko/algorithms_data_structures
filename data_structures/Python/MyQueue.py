348cbbb5ba01ebd7c21b88f6dcb2130cd8d2347e
ZaharChernenko <zaharchernenko35@gmail.com>
2023-12-31 14:13:26 +0300

    iterator for MyQueue

--- data_structures/Python/MyQueue.py
+++ data_structures/Python/MyQueue.py
@@ -1,152 +1,155 @@
+from __future__ import annotations
+
+
 class MyQueue:
     """Очередь на основе односвязного списка, операции по добавлению элементов
-    в начало и конец имеет сложность O(1),
-    остальные - O(n)"""
-    class __Node:
-        def __init__(self, val=None, next=None):
-            self.val = val
-            self.next = next
+в начало и конец имеет сложность O(1), остальные - O(n)"""
+    class _Node:
+        def __init__(self, obj=None, p_next=None):
+            self.obj = obj
+            self.p_next: MyQueue._Node | None = p_next
 
         def __str__(self):
-            return str(self.val)
-
-        """def __str__(self):
-            return f"Node {{val = {self.val}, next = {self.next}}}" """
+            return f"Node {{obj = {self.obj}, next = {self.p_next}}}"
 
     def __init__(self, *args):
-        if not args:
-            self.__front = self.__back = None
-            self.__size = 0
-        else:
-            self.__front = self.__back = self.__Node(args[0])
-            self.__size = 1
-            for i in range(1, len(args)):
-                self.pushBack(args[i])
+        self._front = self._back = None
+        self._size = 0
+        for arg in args:
+            self.pushBack(arg)
 
     def __str__(self):
         return str(self.toList())
 
-    """def __str__(self):
-        return self.__head.__str__()"""
-
     def __len__(self):
-        return self.__size
+        return self._size
 
-    def __getitem__(self, item):
-        if item < 0:
-            item += self.__size
+    def __getitem__(self, index):
+        if index < 0:
+            index += self._size
 
-        if item >= self.__size:
-            raise IndexError
+        if index >= self._size or index < 0:
+            raise IndexError("queue assignment index out of range")
 
-        temp = self.__front
-        for _ in range(item):
-            temp = temp.next
-        return temp.val
+        cur_node = self._front
+        for _ in range(index):
+            cur_node = cur_node.p_next
+        return cur_node.obj
 
-    def __setitem__(self, key, value):
-        if key < 0:
-            key += self.__size
+    def __setitem__(self, index, obj):
+        if index < 0:
+            index += self._size
 
-        if key >= self.__size:
-            raise IndexError
+        if index >= self._size or index < 0:
+            raise IndexError("queue assignment index out of range")
 
-        temp = self.__front
-        for _ in range(key):
-            temp = temp.next
-        temp.val = value
+        cur_node = self._front
+        for _ in range(index):
+            cur_node = cur_node.p_next
+        cur_node.obj = obj
 
-    def __delitem__(self, key):
-        if self.__size == 0:
-            raise IndexError
+    def __delitem__(self, index):
+        if index < 0:
+            index += self._size
 
-        if key < 0:
-            key += self.__size
+        if index >= self._size or index < 0:
+            raise IndexError("queue assignment index out of range")
 
-        if key >= self.__size:
-            raise IndexError
-
-        if key == 0:
-            self.__front = self.__front.next
-            if self.__front is None:
-                self.__back = self.__front
+        if index == 0:
+            self._front = self._front.p_next
+            if not self._front:
+                self._back = self._front
         else:
-            temp = self.__front
-            for _ in range(1, key):
-                temp = temp.next
-            temp.next = temp.next.next
-        self.__size -= 1
+            cur_node = self._front
+            for _ in range(index - 1):
+                cur_node = cur_node.p_next
+            cur_node.p_next = cur_node.p_next.p_next
+        self._size -= 1
+
+    def __iter__(self):
+        return self.QueueIterator(self._front)
+
+    class QueueIterator:
+        def __init__(self, front: MyQueue._Node):
+            self.cur_node = MyQueue._Node(None, front)
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.cur_node.p_next is not None:
+                self.cur_node = self.cur_node.p_next
+                return self.cur_node
+            raise StopIteration
 
     def toList(self) -> list:
-        vec = [0] * self.__size
-        cur_node = self.__front
-        for i in range(self.__size):
-            vec[i] = cur_node.val
-            cur_node = cur_node.next
-        return vec
+        res = [0] * self._size
+        cur_node = self._front
+        for i in range(self._size):
+            res[i] = cur_node.obj
+            cur_node = cur_node.p_next
+        return res
 
-    def pushFront(self, val):  # сложность операции O(1)
-        if not self.__front:
-            self.__front = self.__back = self.__Node(val)
+    def pushFront(self, obj):  # сложность операции O(1)
+        if not self._front:
+            self._front = self._back = self._Node(obj)
         else:
-            self.__front = self.__Node(val, self.__front)
-        self.__size += 1
+            self._front = self._Node(obj, self._front)
+        self._size += 1
 
-    def pushBack(self, val):  # сложность операции O(1)
-        if not self.__front:
-            self.__front = self.__back = self.__Node(val)
+    def pushBack(self, obj):  # сложность операции O(1)
+        if not self._front:
+            self._front = self._back = self._Node(obj)
         else:
-            self.__back.next = self.__Node(val)
-            self.__back = self.__back.next
-        self.__size += 1
+            self._back.p_next = self._Node(obj)
+            self._back = self._back.p_next
+        self._size += 1
+
+    def insert(self, index, obj):
+        if index < 0:
+            index += self._size
+        if index == 0:
+            self.pushFront(obj)
+        elif index >= self._size:
+            self.pushBack(obj)
+        else:
+            cur_node = self._front
+            for _ in range(index - 1):
+                cur_node = cur_node.p_next
+            cur_node.p_next = self._Node(obj, cur_node.p_next)
+            self._size += 1
 
     def popFront(self):  # O(1)
-        if self.__size == 0:
-            raise IndexError
-        val = self.__front.val
-        self.__front = self.__front.next
-        self.__size -= 1
-        return val
+        if not self._front:
+            raise IndexError("pop from empty queue")
+        obj = self._front.obj
+        self._front = self._front.p_next
+        if self._front is None:
+            self._back = self._front
+        self._size -= 1
+        return obj
 
     def popBack(self):  # O(n)
-        if self.__size == 0:
-            raise IndexError
-        val = self.__back.val
-        self.__size -= 1
-        if self.__size == 0:
-            self.__front = self.__back = None
+        if not self._back:
+            raise IndexError("pop from empty queue")
+        obj = self._back.obj
+        self._size -= 1
+        if self._size == 0:
+            self._front = self._back = None
         else:
-            temp = self.__back
-            self.__back = self.__front
-            while self.__back.next != temp:
-                self.__back = self.__back.next
-            self.__back.next = None
-        return val
-
-    def insert(self, key, value):
-        if key < 0:
-            key += self.__size
-
-        if key > self.__size:
-            raise IndexError
-
-        if key == 0:
-            self.pushFront(value)
-        elif key == self.__size:
-            self.pushBack(value)
-        else:
-            temp = self.__front
-            for _ in range(1, key):
-                temp = temp.next
-            temp.next = self.__Node(value, temp.next)
-            self.__size += 1
+            temp = self._back
+            self._back = self._front
+            while self._back.p_next != temp:
+                self._back = self._back.p_next
+            self._back.p_next = None
+        return obj
 
     def front(self):
-        if self.__size == 0:
-            raise IndexError
-        return self.__front.val
+        if self._size == 0:
+            raise IndexError("queue is empty")
+        return self._front.obj
 
     def back(self):
-        if self.__size == 0:
-            raise IndexError
-        return self.__back.val
+        if self._size == 0:
+            raise IndexError("queue is empty")
+        return self._back.obj
--- data_structures/Python/main.py
+++ data_structures/Python/main.py
@@ -1,9 +1,61 @@
+import timeit
+from queue import Queue
+
 from BinaryTree import BinaryTree
 from MyQueue import MyQueue
 
-queue = MyQueue(1, 2, 3, 4, 5)
-queue.insert(1, 123)
-print(queue)
 
-tree = BinaryTree(4, 3, 2, 1, 5, 6, 7, 8)
-print(tree)
+def timer(func, *args, **kwargs):
+    """measures the time of one function call"""
+
+    start = timeit.time.time_ns()
+    func(*args, **kwargs)
+    elapsed = timeit.time.time_ns() - start
+    print(f"Время выполнения в ns: {elapsed:,d}")
+
+
+def timerRepeat(func, repeat: int, *args, **kwargs):
+    start = timeit.time.time_ns()
+    for _ in range(repeat):
+        func(*args, **kwargs)
+    elapsed = timeit.time.time_ns() - start
+    print(f"Время выполнения в ns: {elapsed:,d}")
+    return elapsed
+
+
+def bestOf(func, repeat: int, *args, **kwargs):
+    fastest_run = float("inf")
+    for _ in range(repeat):
+        start = timeit.time.time_ns()
+        func(*args, **kwargs)
+        elapsed = timeit.time.time_ns() - start
+        fastest_run = min(fastest_run, elapsed)
+    print(f"Самое быстрое время в ns: {fastest_run:,}")
+    return fastest_run
+
+
+def bestOfTotal(func, repeat: int, loops: int, *args, **kwargs):
+    fastest_run = float("inf")
+    for _ in range(repeat):
+        elapsed = timerRepeat(func, loops, *args, **kwargs)
+        fastest_run = min(fastest_run, elapsed)
+    print(f"{loops} циклов, лучшее время из {repeat} в ns: {fastest_run:,d}")
+
+
+# timer(MyQueue, 1, 2, 3, 4, 5)
+# timerRepeat(MyQueue, 1000, 1, 2, 3, 4, 5)
+# bestOf(MyQueue, 100, 1, 2, 3, 4, 5, 6)
+# bestOfTotal(MyQueue, 10, 1000, 1, 2, 3, 4, 5)
+# print(min(timeit.repeat(repeat=10, number=1000, stmt="MyQueue(1, 2, 3, 4, 5)", setup="from MyQueue import MyQueue")))
+# print(min(timeit.repeat(repeat=10, number=1000, stmt="MyQueue(1, 2, 3, 4, 5)", globals=globals())))
+# с globals не работает нижняя команда
+# python3 -m timeit "$(cat main.py)"
+
+
+q = MyQueue(1, 2, 3, 4, 5)
+for elem in q:
+    elem = 2
+print(q)
+
+q = Queue()
+print(dir(q))

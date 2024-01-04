d6ac0cc08f12cf2e7e9ff9a627ebdc24900a1200
ZaharChernenko <zaharchernenko35@gmail.com>
2024-01-04 22:25:09 +0300

    added generator function for dec mul

--- algorithms/MFTI python/lecture_8 Generate permutations.py
+++ algorithms/MFTI python/lecture_8 Generate permutations.py
@@ -8,7 +8,7 @@ def _genBin(prefix: list, step: int, temp_arr: list):
 
 
 def genBin(length) -> list:
-    temp_arr = []
+    temp_arr: list = []
     _genBin([None] * length, 0, temp_arr)
     return temp_arr
 
@@ -24,7 +24,7 @@ def _genNumbers(base: int, prefix: list, step: int, temp_arr: list):
 
 
 def genNumbers(length: int, base: int) -> list:
-    temp_arr = []
+    temp_arr: list = []
     _genNumbers(base, [None] * length, 0, temp_arr)
     return temp_arr
 
@@ -35,6 +35,8 @@ def genNumbers(length: int, base: int) -> list:
 лишь один раз, если эту функцию вызвать дважды, то она будет
 содержать и значения первого вызова
 """
+
+
 def generatePermutationsBad(length: int, elements_list: list, prefix="", arr=[]):
     assert len(elements_list) >= length, "Необходимо, чтобы число элементов было не меньше длины последовательности"
     if length == 0:
@@ -64,7 +66,7 @@ def _genUniqPer(uniq: set, prefix: list, step: int, chosen: set, temp_arr: list)
 def genUniqPer(uniq: set, length: int) -> list:
     assert length <= len(uniq), "length must be not bigger than set"
 
-    temp_arr = []
+    temp_arr: list = []
     _genUniqPer(uniq, [None] * length, 0, set(), temp_arr)
     return temp_arr
 
@@ -82,7 +84,7 @@ def Dec(arr, i=0, a=None, prefix=None):
     return a
 
 
-def _genDecart(arr2d: list[list], prefix, step: int, temp_arr: list):
+def _listDecart(arr2d: list[list], prefix, step: int, temp_arr: list):
     if step == len(arr2d):
         "important to copy prefix, because its mutable"
         temp_arr.append(list(prefix))
@@ -90,13 +92,29 @@ def _genDecart(arr2d: list[list], prefix, step: int, temp_arr: list):
 
     for number in arr2d[step]:
         prefix[step] = number
-        _genDecart(arr2d, prefix, step + 1, temp_arr)
+        _listDecart(arr2d, prefix, step + 1, temp_arr)
 
 
-def genDecart(arr2d: list[list]) -> list[list]:
-    temp_arr = []
-    _genDecart(arr2d, [None] * len(arr2d), 0, temp_arr)
+def listDecart(arr2d: list[list]) -> list[list]:
+    temp_arr: list = []
+    _listDecart(arr2d, [None] * len(arr2d), 0, temp_arr)
     return temp_arr
+
+
+def _genDecart(arr2d: list[list], prefix, step: int):
+    if step == len(arr2d):
+        "important to copy prefix, because its mutable"
+        yield tuple(prefix)
+        return
+    for number in arr2d[step]:
+        prefix[step] = number
+        yield from _genDecart(arr2d, prefix, step + 1)
+
+
+def genDecart(arr2d: list[list]):
+    gen = _genDecart(arr2d, [None] * len(arr2d), 0)
+    for elem in gen:
+        yield elem
 
 
 for vector in genDecart([[1, 2, 3], [4, 5, 6], [7, 8, 9]]):

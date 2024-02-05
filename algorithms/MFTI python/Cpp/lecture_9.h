7852b05f2101fd092c3607fb914251c0d9666899
ZaharChernenko <zaharchernenko35@gmail.com>
2024-02-06 00:55:31 +0300

    refactored lecture_9.h

--- algorithms/MFTI python/Cpp/lecture_9.h
+++ algorithms/MFTI python/Cpp/lecture_9.h
@@ -1,25 +1,21 @@
 #pragma once
+#include <cstddef>
+#include <cstdlib>
+#include <utility>
 #include <vector>
-using std::cin, std::cout;
 
 
-template <typename T>
-void merge(std::vector<T>& vec, const std::vector<T>& left, const std::vector<T>& right) {
-    int i1 = 0, i2 = 0, i = 0;
-    while (i1 < left.size() && i2 < right.size()) {
-        if (left[i1] <= right[i2]) {
-            vec[i++] = left[i1++];
-        }
-        else {
-            vec[i++] = right[i2++];
-        }
+template <class T>
+void merge(const std::vector<T>& left, const std::vector<T>& right, std::vector<T>& arr) {
+    std::size_t left_index {0}, right_index {0}, arr_index {0};
+
+    while (left_index != left.size() && right_index != right.size()) {
+        if (left[left_index] <= right[right_index]) arr[arr_index++] = left[left_index++];
+        else arr[arr_index++] = right[right_index++];
     }
-    while (i1 < left.size()) {
-        vec[i++] = left[i1++];
-    }
-    while (i2 < right.size()) {
-        vec[i++] = right[i2++];
-    }
+
+    while (left_index != left.size()) arr[arr_index++] = left[left_index++];
+    while (right_index != right.size()) arr[arr_index++] = left[right_index++];
 }
 
 
@@ -34,32 +30,35 @@ void mergeSort(std::vector<T>& vec) {
 }
 
 
-template <typename T>
-void hoarSort(std::vector<T>& vec) {
-    if (vec.size() <= 1) {
-        return;
+template <class T>
+std::size_t partition(std::vector<T>& arr, const std::size_t& left, const std::size_t& right) {
+    std::size_t equal_index {left}, greater_index {left};
+    T pivot {arr[std::rand() % (right - left) + left]};
+
+    for (std::size_t i = left; i != right; ++i) {
+        if (arr[i] < pivot) {
+            std::swap(arr[greater_index], arr[i]);
+            std::swap(arr[equal_index++], arr[greater_index++]);
+        }
+        else if (arr[i] == pivot) std::swap(arr[greater_index++], arr[i]);
     }
-    T barrier = vec[0];
-    std::vector<T> left, middle, right;
-    for (const auto& el: vec) {
-        if (el < barrier) left.push_back(el);
-        else if (el == barrier) middle.push_back(el);
-        else right.push_back(el);
-    }
-    hoarSort(left);
-    hoarSort(right);
-    vec.clear();
-    vec.insert(vec.begin(), left.begin(), left.end());
-    vec.insert(vec.end(), middle.begin(), middle.end());
-    vec.insert(vec.end(), right.begin(), right.end());
+
+    return equal_index;
 }
 
 
-void testS() {
-    std::vector<int> vec{5, 2, 4, 1, 99, 23, 0};
-    mergeSort(vec);
-    for (auto el: vec) {
-        cout << el << '\n';
+template <class T>
+void _hoarSort(std::vector<T>& arr, const std::size_t& left, const std::size_t& right) {
+    if (left < right) {
+        std::size_t sorted_index = partition(arr, left, right);
+        _hoarSort(arr, left, sorted_index);
+        _hoarSort(arr, sorted_index + 1, right);
     }
+}
+
+
+template <class T>
+void hoarSort(std::vector<T> &arr) {
+    _hoarSort(arr, 0, arr.size());
 }
 

d4c62b4aee32d6f25107bec44b787e7ab7ada5a9
ZaharChernenko <zaharchernenko35@gmail.com>
2024-08-27 10:32:27 +0300

    renewed lecture_9

--- algorithms/MFTI python/Cpp/lecture_7 Recursion.h
+++ algorithms/MFTI python/Cpp/lecture_7 Recursion.h
@@ -1,11 +1,10 @@
 #include <iostream>
 
-
 long long fib(int n) {
-    if (n == 1 || n == 2) return 1;
+    if (n == 1 || n == 2)
+        return 1;
     return fib(n - 1) + fib(n - 2);
 }
-
 
 void matreshka(int n) {
     if (!n) {
@@ -17,32 +16,33 @@ void matreshka(int n) {
     std::cout << "Низ матрешки: n = " << n << '\n';
 }
 
-
 int pow(int num, int degree) {
-    if (!degree) return 1;
+    if (!degree)
+        return 1;
 
     return pow(num, degree - 1) * num;
 }
 
-
 int fasterPow(int num, int degree) {
-    if (!degree) return 1;
+    if (!degree)
+        return 1;
 
-    if (degree % 2 == 0) return fasterPow(num * num, degree / 2);
+    if (degree % 2 == 0)
+        return fasterPow(num * num, degree / 2);
     return fasterPow(num, degree - 1) * num;
 }
 
-
 int gcd(int a, int b) {
-    if (a == b) return a;
+    if (a == b)
+        return a;
 
-    if (a > b) return gcd(a - b, b);
+    if (a > b)
+        return gcd(a - b, b);
     return gcd(a, b - a);
 }
 
-
 int fasterGCD(int a, int b) {
-    if (b == 0) return a;
+    if (b == 0)
+        return a;
     return fasterGCD(b, a % b);
 }
-
--- algorithms/MFTI python/Python/lecture_9 Merge sort.py
+++ algorithms/MFTI python/Python/lecture_9 Merge sort.py
@@ -1,5 +1,6 @@
 from random import randrange
 
+
 def merge(left: list, right: list) -> list:
     temp_arr = [None] * (len(left) + len(right))
     left_i = right_i = temp_i = 0
@@ -25,33 +26,34 @@ def merge(left: list, right: list) -> list:
 
 def mergeSort(arr: list) -> list:
     "Сортировка слиянием"
-    return arr if len(arr) <= 1 else merge(mergeSort(arr[:len(arr) // 2]), mergeSort(arr[len(arr) // 2:]))
+    return arr if len(arr) <= 1 else merge(mergeSort(arr[: len(arr) // 2]), mergeSort(arr[len(arr) // 2 :]))
 
 
-def partition(arr: list, left: int, right: int) -> int:
-    pivot = arr[randrange(left, right)]
-    equal = greater = left
+def partition[T](arr: list[T], left: int, right: int) -> int:
+    pivot: T = arr[randrange(left, right)]
+    equal_index = greater_index = left
 
-    for i in range(left, right):
-        if arr[i] < pivot:
-            arr[i], arr[greater] = arr[greater], arr[i]
-            arr[equal], arr[greater] = arr[greater], arr[equal]
-            greater += 1
-            equal += 1
-        elif arr[i] == pivot:
-            arr[greater], arr[i] = arr[i], arr[greater]
-            greater += 1
-    return equal
+    for index in range(left, right):
+        if arr[index] < pivot:
+            arr[greater_index], arr[index] = arr[index], arr[greater_index]
+            arr[greater_index], arr[equal_index] = arr[equal_index], arr[greater_index]
+            equal_index += 1
+            greater_index += 1
+        elif arr[index] == pivot:
+            arr[greater_index], arr[index] = arr[index], arr[greater_index]
+            greater_index += 1
 
-
-def _quickSort(arr: list, left: int, right: int) -> None:
-    if left < right:
-        sorted_elem_index = partition(arr, left, right)
-        _quickSort(arr, left, sorted_elem_index)
-        _quickSort(arr, sorted_elem_index + 1, right)
+    return equal_index
 
 
-def quickSort(arr: list):
+def _quickSort[T](arr: list[T], left: int, right: int):
+    if left + 1 < right:
+        sorted_index: int = partition(arr, left, right)
+        _quickSort(arr, left, sorted_index)
+        _quickSort(arr, sorted_index + 1, right)
+
+
+def quickSort[T](arr: list[T]):
     _quickSort(arr, 0, len(arr))
 
 
